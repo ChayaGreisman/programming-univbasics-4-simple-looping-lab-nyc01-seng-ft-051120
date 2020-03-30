8184964cd97add951d6206ce7ddbef1012507c1e
Chaya Greisman <chayamgreisman@gmail.com>
2020-03-30 18:29:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,11 @@ def loop_message_n_times(string,n)
     count += 1
   end
 end
+
+def output_array(array)
+  count = 0 
+  while array[counts] do 
+    puts array[counts]
+    count += 1
+  end
+end
